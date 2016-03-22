287f3b29aaebf27e6b7eebbecae58a259097897b
Timotius Margo <timargo@microsoft.com>
2016-03-21 20:40:16 -0700

    Upload sample tests tailored for UWP apps, system apps, and mixed scenario

---
+++ Samples/AlarmsClockTest/Scenario.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.iOS; // Temporary placeholder until Windows namespace exists
+using OpenQA.Selenium.Remote;
+
+namespace AlarmsClockTest
+{
+    [TestClass]
+    public class Scenario
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        protected static IOSDriver<IOSElement> AlarmsClockSession;  // Temporary placeholder until Windows namespace exists
+        protected static IOSDriver<IOSElement> DesktopSession;      // Temporary placeholder until Windows namespace exists
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            // Launch the AlarmsClock app
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+            AlarmsClockSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(AlarmsClockSession);
+            AlarmsClockSession.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+
+            // Create a session for Desktop
+            DesiredCapabilities desktopCapabilities = new DesiredCapabilities();
+            desktopCapabilities.SetCapability("app", "Root");
+            DesktopSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), desktopCapabilities);
+            Assert.IsNotNull(DesktopSession);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            ReturnToMainPage();
+            SwitchToAlarmTab();
+
+            // Delete all created alarms
+            var alarmEntries = AlarmsClockSession.FindElementsByName("Windows Application Driver Test Alarm");
+            foreach (var alarmEntry in alarmEntries)
+            {
+                AlarmsClockSession.Mouse.ContextClick(alarmEntry.Coordinates);
+                AlarmsClockSession.FindElementByName("Delete").Click();
+            }
+
+            AlarmsClockSession.Dispose();
+            AlarmsClockSession = null;
+        }
+
+        [TestMethod]
+        public void FullScenario()
+        {
+            // Read the current local time
+            SwitchToWorldClockTab();
+            string localTimeText = ReadLocalTime();
+            Assert.IsTrue(localTimeText.Length > 0);
+
+            // Add an alarm at 1 minute after local time
+            SwitchToAlarmTab();
+            AddAlarm(localTimeText);
+            var alarmEntries = AlarmsClockSession.FindElementsByName("Windows Application Driver Test Alarm");
+            Assert.IsTrue(alarmEntries.Count > 0);
+
+            // Try to dismiss notification after 1 minute
+            System.Threading.Thread.Sleep(60000);
+            DismissNotification();
+        }
+
+        public static void SwitchToAlarmTab()
+        {
+            AlarmsClockSession.FindElementByAccessibilityId("AlarmPivotItem").Click();
+        }
+
+        public void SwitchToWorldClockTab()
+        {
+            AlarmsClockSession.FindElementByAccessibilityId("WorldClockPivotItem").Click();
+        }
+
+        public string ReadLocalTime()
+        {
+            string localTimeText = "";
+            AppiumWebElement worldClockPivotItem = AlarmsClockSession.FindElementByAccessibilityId("WorldClockPivotItem");
+            if (worldClockPivotItem != null)
+            {
+                localTimeText = worldClockPivotItem.FindElementByClassName("ClockCardItem").Text;
+                var timeStrings = localTimeText.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (timeStrings.Length >= 5)
+                {
+                    // Get the time. E.g. "Local time, Monday, February 22, 2016, 11:32 AM, "
+                    localTimeText = timeStrings[4];
+                }
+            }
+
+            return localTimeText;
+        }
+
+        public void AddAlarm(string timeText)
+        {
+            if (timeText.Length > 0)
+            {
+                // Create a test alarm 1 minute after the read local time
+                string alarmTimeString = timeText;
+                DateTimeFormatInfo fi = new CultureInfo("en-US", false).DateTimeFormat;
+                DateTime alarmTime = DateTime.ParseExact(alarmTimeString, " h:mm tt", fi);
+                alarmTime = alarmTime.AddMinutes(1.0);
+                string hourString = alarmTime.ToString("%h", fi);
+                string minuteString = alarmTime.ToString("mm", fi);
+                string period = alarmTime.ToString("tt", fi);
+
+                AlarmsClockSession.FindElementByAccessibilityId("AddAlarmButton").Click();
+                AlarmsClockSession.FindElementByAccessibilityId("AlarmNameTextBox").Clear();
+                AlarmsClockSession.FindElementByAccessibilityId("AlarmNameTextBox").SendKeys("Windows Application Driver Test Alarm");
+                AlarmsClockSession.FindElementByAccessibilityId("AlarmTimePicker").FindElementByAccessibilityId("FlyoutButton").Click();
+                AlarmsClockSession.FindElementByAccessibilityId("HourLoopingSelector").FindElementByName(hourString).Click();
+                AlarmsClockSession.FindElementByAccessibilityId("MinuteLoopingSelector").FindElementByName(minuteString).Click();
+                AlarmsClockSession.FindElementByAccessibilityId("PeriodLoopingSelector").FindElementByName(period).Click();
+                AlarmsClockSession.FindElementByName("timepicker").FindElementByAccessibilityId("AcceptButton").Click();
+                AlarmsClockSession.FindElementByAccessibilityId("AlarmSaveButton").Click();
+            }
+        }
+
+        public void DismissNotification()
+        {
+            try
+            {
+                AppiumWebElement newNotification = DesktopSession.FindElementByName("New notification");
+                Assert.IsTrue(newNotification.FindElementByAccessibilityId("MessageText").Text.Contains("Windows Application Driver Test Alarm"));
+                newNotification.FindElementByName("Dismiss").Click();
+            }
+            catch { }
+        }
+
+        private static void ReturnToMainPage()
+        {
+            // Try to return to main page in case application is started in nested view
+            try
+            {
+                AppiumWebElement backButton = null;
+                do
+                {
+                    backButton = AlarmsClockSession.FindElementByAccessibilityId("Back");
+                    backButton.Click();
+                }
+                while (backButton != null);
+            }
+            catch { }
+        }
+    }
+}
---
+++ Samples/CortanaTest/Scenario.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.iOS; // Temporary placeholder until Windows namespace exists
+using OpenQA.Selenium.Remote;
+
+namespace CortanaTest
+{
+    [TestClass]
+    public class Scenario
+    {
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        protected static IOSDriver<IOSElement> CortanaSession;      // Temporary placeholder until Windows namespace exists
+        protected static IOSDriver<IOSElement> DesktopSession;      // Temporary placeholder until Windows namespace exists
+        protected static AppiumWebElement CortanaButton;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            // Create a session for Desktop
+            DesiredCapabilities desktopCapabilities = new DesiredCapabilities();
+            desktopCapabilities.SetCapability("app", "Root");
+            DesktopSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), desktopCapabilities);
+            Assert.IsNotNull(DesktopSession);
+
+            // Launch Cortana Window to allow session creation to find it
+            CortanaButton = DesktopSession.FindElementByAccessibilityId("4101"); // AutomationId for Cortana button on the task bar
+            CortanaButton.Click();
+            System.Threading.Thread.Sleep(1000);
+
+            // Create session for already running Cortana
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", "Microsoft.Windows.Cortana_cw5n1h2txyewy!CortanaUI");
+            CortanaSession = new IOSDriver<IOSElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(CortanaSession);
+            CortanaSession.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            CortanaButton = null;
+            CortanaSession.Dispose();
+            CortanaSession = null;
+            DesktopSession.Dispose();
+            DesktopSession = null;
+        }
+
+        [TestInitialize]
+        public void OpenCortanaWindow()
+        {
+            if (CortanaButton != null)
+            {
+                CortanaButton.Click();
+            }
+        }
+
+        [TestMethod]
+        public void BingLocalSearch()
+        {
+            var searchBox = CortanaSession.FindElementByAccessibilityId("SearchTextBox");
+            Assert.IsNotNull(searchBox);
+            searchBox.SendKeys("add");
+
+            var bingPane = CortanaSession.FindElementByName("Bing");
+            Assert.IsNotNull(bingPane);
+
+            var bingResult = bingPane.FindElementByName("Add or remove programs, System settings");
+            Assert.IsNotNull(bingResult);
+        }
+
+        [TestMethod]
+        public void BingRemoteSearch()
+        {
+            var searchBox = CortanaSession.FindElementByAccessibilityId("SearchTextBox");
+            Assert.IsNotNull(searchBox);
+            searchBox.SendKeys("What is eight times eleven");
+
+            var bingPane = CortanaSession.FindElementByName("Bing");
+            Assert.IsNotNull(bingPane);
+
+            var bingResult = bingPane.FindElementByName("What is eight times eleven");
+            Assert.IsNotNull(bingResult);
+        }
+
+        [TestMethod]
+        public void MenuBarScenario()
+        {
+            var settingsListItem = CortanaSession.FindElementByAccessibilityId("SettingsListItem");
+            Assert.IsNotNull(settingsListItem);
+
+            var settingsListItemLabel = settingsListItem.FindElementByAccessibilityId("TextLabel");
+            Assert.IsNotNull(settingsListItem);
+            Assert.IsFalse(settingsListItemLabel.Displayed);
+
+            CortanaSession.FindElementByAccessibilityId("HomeburgerTopItem").Click();
+            System.Threading.Thread.Sleep(1000);
+            Assert.IsTrue(settingsListItemLabel.Displayed);
+
+            CortanaSession.FindElementByAccessibilityId("SettingsListItem").Click();
+            var clearAllButton = CortanaSession.FindElementByAccessibilityId("DeviceSearchHistoryClearButton");
+            Assert.IsNotNull(clearAllButton);
+            Assert.IsTrue(clearAllButton.Displayed);
+        }
+    }
+}
