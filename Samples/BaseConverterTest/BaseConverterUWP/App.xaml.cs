129934af1ac32e7c88334dc14b4fd0bf63c2d101
alakshmi1030 <anlak@microsoft.com>
2016-04-15 15:42:18 -0700

    Modified namespace and classname

--- Samples/BaseConverterTest/BaseConverterUWP/App.xaml.cs
+++ Samples/BaseConverterTest/BaseConverterUWP/App.xaml.cs
@@ -72,7 +72,7 @@ namespace TutorialApp
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(BaseConverterUWP.MainPage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
