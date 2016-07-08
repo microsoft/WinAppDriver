9ceadfc51e7d88030c3140addbf7302b194c2aef
Paul Campbell (CMD) <paulcam@microsoft.com>
2016-07-08 16:05:41 -0700

    Fix the Calculator test to change/restore the calculator mode on setup/teardown

--- Samples/C#/CalculatorTest/BasicScenarios.cs
+++ Samples/C#/CalculatorTest/BasicScenarios.cs
@@ -26,6 +26,7 @@ namespace CalculatorTest
         protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
         protected static RemoteWebDriver CalculatorSession;
         protected static RemoteWebElement CalculatorResult;
+        protected static string OriginalCalculatorMode;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
@@ -36,6 +37,11 @@ namespace CalculatorTest
             CalculatorSession = new RemoteWebDriver(new Uri(WindowsApplicationDriverUrl), appCapabilities);
             Assert.IsNotNull(CalculatorSession);
             CalculatorSession.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+
+            // Make sure we're in standard mode
+            CalculatorSession.FindElementByXPath("//Button[starts-with(@Name, \"Menu\")]").Click();
+            OriginalCalculatorMode = CalculatorSession.FindElementByXPath("//List[@AutomationId=\"FlyoutNav\"]//ListItem[@IsSelected=\"True\"]").Text;
+            CalculatorSession.FindElementByXPath("//ListItem[@Name=\"Standard Calculator\"]").Click();
 
             // Use series of operation to locate the calculator result text element as a workaround
             // We currently cannot query element by automationId without using modified appium dot net driver
@@ -49,6 +55,9 @@ namespace CalculatorTest
         [ClassCleanup]
         public static void TearDown()
         {
+            CalculatorSession.FindElementByXPath("//Button[starts-with(@Name, \"Menu\")]").Click();
+            CalculatorSession.FindElementByXPath("//ListItem[@Name=\"" + OriginalCalculatorMode + "\"]").Click();
+
             CalculatorResult = null;
             CalculatorSession.Dispose();
             CalculatorSession = null;
