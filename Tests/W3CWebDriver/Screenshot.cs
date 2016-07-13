1f2426b152faaf50d1708ca18cded0ba5b924fcc
Paul Campbell (CMD) <paulcam@microsoft.com>
2016-07-13 14:37:12 -0700

    Add screenshot and source tests to w3cwebdriver

---
+++ Tests/W3CWebDriver/Screenshot.cs
@@ -0,0 +1,74 @@
+﻿//******************************************************************************
+//
+// Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
+
+namespace W3CWebDriver
+{
+    [TestClass]
+    public class Screenshot
+    {
+        protected static IOSDriver<IOSElement> session;
+        protected static IOSElement alarmTabElement;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", CommonTestSettings.AlarmClockAppId);
+            session = new IOSDriver<IOSElement>(new Uri(CommonTestSettings.WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(session);
+            Assert.IsNotNull(session.SessionId);
+
+            alarmTabElement = session.FindElementByAccessibilityId("AlarmPivotItem");
+            Assert.IsNotNull(alarmTabElement);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+
+        [TestMethod]
+        public void GetScreenshot()
+        {
+            string screenshotName = "screenshot.png";
+            var screenshot = session.GetScreenshot();
+
+            try
+            {
+                screenshot.SaveAsFile(screenshotName, ImageFormat.Png);
+                Assert.IsTrue(File.Exists(screenshotName));
+                FileInfo screenshotInfo = new FileInfo(screenshotName);
+                Assert.IsTrue(screenshotInfo.Length > 0);
+            }
+            finally
+            {
+                File.Delete(screenshotName);
+            }
+        }
+    }
+}
