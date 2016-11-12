bf2011c0d7fcac16dfffdada8f800e51ed84105f
Timotius Margo <timargo@microsoft.com>
2016-11-11 16:04:27 -0800

    Add unit tests for /session/:sessionId/orientation API endpoint

---
+++ Tests/W3CWebDriver/Orientation.cs
@@ -0,0 +1,70 @@
+﻿//******************************************************************************
+//
+// Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Remote;
+
+namespace W3CWebDriver
+{
+    [TestClass]
+    public class Orientation
+    {
+        private IOSDriver<IOSElement> session = null;
+
+        [TestMethod]
+        public void GetOrientation()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", CommonTestSettings.CalculatorAppId);
+            session = new IOSDriver<IOSElement>(new Uri(CommonTestSettings.WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(session);
+            Assert.IsNotNull(session.SessionId);
+
+            var orientation = session.Orientation;
+            Assert.IsNotNull(orientation);
+            Assert.IsTrue(orientation == OpenQA.Selenium.ScreenOrientation.Landscape || orientation == OpenQA.Selenium.ScreenOrientation.Portrait);
+
+            session.Quit();
+            session = null;
+        }
+
+        [TestMethod]
+        public void ErrorGetOrientationNoSuchWindow()
+        {
+            DesiredCapabilities appCapabilities = new DesiredCapabilities();
+            appCapabilities.SetCapability("app", CommonTestSettings.CalculatorAppId);
+            session = new IOSDriver<IOSElement>(new Uri(CommonTestSettings.WindowsApplicationDriverUrl), appCapabilities);
+            Assert.IsNotNull(session);
+            Assert.IsNotNull(session.SessionId);
+
+            try
+            {
+                session.Close();
+                var orientation = session.Orientation;
+                Assert.Fail("Exception should have been thrown");
+            }
+            catch (System.InvalidOperationException exception)
+            {
+                Assert.AreEqual("Currently selected window has been closed", exception.Message);
+            }
+
+            session.Quit();
+            session = null;
+        }
+    }
+}
