89c6726c891f3efc9af3dd4dc31cc238a20a2d70
Ian Ceicys <ian.ceicys@autodesk.com>
2016-12-05 16:11:18 -0500

    Updating Test to simplify repo steps

--- Samples/C#/WordTest/Scenario.cs
+++ Samples/C#/WordTest/Scenario.cs
@@ -2,6 +2,7 @@ using System;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Appium.iOS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
 
 namespace WordTest
 {
@@ -9,8 +10,9 @@ namespace WordTest
     public class Scenario
     {
         protected const string AppDriverUrl = "http://127.0.0.1:4723";
-        protected const string TextValue = "This is an automated test on Classic Windows Application!";
+        protected const string TextValue = "This is an automated test on Microsoft Office 2016 Word!";
         protected const string WordAppId = @"C:\Program Files (x86)\Microsoft Office\root\Office16\WINWORD.EXE";
+        //protected const string WordAppId = @"C:\Program Files(x86)\admindirectory\wpfnotepad.exe";        
         protected const string ExplorerAppId = @"C:\Windows\System32\explorer.exe";
         protected const string TestFileName = "NotepadTestOutputFile.txt";
         protected const string TargetSaveLocation = @"%TEMP%\";
@@ -24,6 +26,7 @@ namespace WordTest
             appCapabilities.SetCapability("app", WordAppId);
             WordSession = new IOSDriver<IOSElement>(new Uri(AppDriverUrl), appCapabilities);
             Assert.IsNotNull(WordSession);
+            Thread.Sleep(30000);
         }
         [TestMethod]
         public void BasicScenario()
