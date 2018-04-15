8fd6a08bb871ce3510e32659d4f33e8c96c32ad1
Timotius Margo <timargo@microsoft.com>
2018-04-14 18:17:11 -0700

    Create a sample test to demonstrate pen actions using built-in Sticky Notes application Add scenario to draw simple basic pen strokes as the main basic example Add scenario to draw strokes with extra attributes such as pressure, twist, and tilt angle Add scenario to erase previously drawn strokes using the pen eraser Create README.md to provide more details on how to perform various pen actions

---
+++ Samples/C#/StickyNotesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+//******************************************************************************
+//
+// Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+//
+// This code is licensed under the MIT License (MIT).
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//******************************************************************************
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StickyNotesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StickyNotesTest")]
+[assembly: AssemblyCopyright("Copyright © 2018 Microsoft Corporation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ed00387-c536-4ad7-9228-9fb87d3a2fd7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
